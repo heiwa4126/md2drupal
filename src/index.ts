66e7a5027ccb145240e0e25e3b98d9c08c1eb19b
heiwa4126 <heiwa4126@gmail.com>
2025-11-25 15:47:04 +0900

    Add AI coding agent instructions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { Command } from "commander";
 import path from "node:path";
+import pkg from "../package.json" with { type: "json" };
 import convertMarkdownToHTML from "./converter1.js";
 
 const program = new Command();
@@ -9,7 +10,7 @@ const program = new Command();
 program
 	.name("md2drupal")
 	.description("Convert Markdown files to Drupal-compatible HTML")
-	.version("0.0.5-alpha1")
+	.version(`md2drupal v${pkg.version}`)
 	.argument("<input-file>", "Markdown file to convert")
 	.option("-o, --output <file>", "Output HTML file path")
 	.action((inputFile: string, options: { output?: string }) => {
