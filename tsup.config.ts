4c6781a35104a7cfec7355db5c3986cd77ad9b6e
heiwa4126 <heiwa4126@gmail.com>
2025-11-25 13:50:31 +0900

    [WIP] backup. tsc->tsup

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+	format: ["esm", "cjs"],
+	outDir: "dist",
+	bundle: false,
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+	dts: {
+		resolve: true,
+		entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+	},
+});
