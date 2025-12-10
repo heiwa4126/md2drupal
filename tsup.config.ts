50f770e1dea68c9ec8d1a923fe41c11b2cbc116b
heiwa4126 <heiwa4126@gmail.com>
2025-12-10 11:37:57 +0900

    add small cleanups for newxt release

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-	entry: ["src/**/*.ts", "!src/**/*.test.ts"],
-	format: ["esm", "cjs"],
-	outDir: "dist",
-	bundle: false,
-	splitting: false,
-	sourcemap: true,
-	clean: true,
-	dts: {
-		resolve: true,
+export default defineConfig([
+	{
+		entry: ["src/**/*.ts", "!src/index.ts", "!src/**/*.test.ts"],
+		format: ["esm", "cjs"],
+		outDir: "dist",
+		bundle: false,
+		splitting: false,
+		sourcemap: false,
+		clean: true,
+		dts: true,
 	},
-});
+	{
+		entry: ["src/index.ts"],
+		format: ["esm"],
+		outDir: "dist",
+		bundle: false,
+		splitting: false,
+		sourcemap: false,
+		clean: false,
+		minify: true,
+		dts: true,
+	},
+]);
